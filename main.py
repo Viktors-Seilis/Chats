beb3643fad48995a5d273b4d67c398d5d563fb11
Viktors-Seilis <vikijs10@gmail.com>
2020-02-28 15:00:23 +0000

    with main 2

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from flask import Flask, json, jsonify, render_template, request
 
-app = Flask('app')
+app = Flask(__name__)
 
 
 @app.route('/')
 def index_lapa():
-  return render_template('chats.html')
-
-
-@app.route('/health')
-def health_check():
-  return "OK"
+  return render_template('index.html', aktiva_lapa ="home")
 
 
 @app.route('/chats/lasi')
@@ -20,7 +15,6 @@ def ielasit_chatu():
     for rinda in f:
       chata_rindas.append(rinda)
   return jsonify({"chats": chata_rindas})
-
 
 @app.route('/chats/suuti', methods=['POST'])
 def suutiit_zinju():
@@ -34,4 +28,4 @@ def suutiit_zinju():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(host='0.0.0.0', port = 5000, threaded = True, debug = True)
